aa2f74a99ee2b8a083066018081f73d2a243e933
Ralf-Renz-Estellana-Bantilo <ralfrenzbantilo853@gmail.com>
2023-09-23 21:16:05 +0800

    redo changes

--- pages/components/Personal.tsx
+++ pages/components/Personal.tsx
@@ -1,9 +1,8 @@
-import { Divider, Timeline } from 'antd'
+import { Timeline } from 'antd'
 import React, { useState } from 'react'
 import { Quicksand } from 'next/font/google'
 import Image from 'next/image'
 import { Variants, motion } from "framer-motion";
-import { setTimestamp } from '../utils/Resources';
 import Container from './Container';
 
 const quicksand = Quicksand( { subsets: ['latin'] } )
@@ -193,6 +192,40 @@ const Personal = () =>
             </>
          ),
       }
+   }
+
+   const setTimestamp = ( mm: number, yyyy: number ): string =>
+   {
+      let result: string = ''
+      let stampNumber: number = 0
+      let currentMonth: number = new Date().getMonth() + 1
+      let currentYear: number = new Date().getFullYear()
+
+      let stampString: string = ''
+
+      if ( ( yyyy > currentYear && mm > currentMonth ) || ( yyyy === currentYear && mm > currentMonth ) || ( yyyy > currentYear && mm === currentMonth ) )
+      {
+         return `Invalid TimeStamp!`
+      }
+
+      if ( currentYear > yyyy && currentMonth < mm )
+      {
+         stampNumber = ( currentMonth + 12 ) - mm
+         stampString = stampNumber > 1 ? 'Months' : 'Month'
+      } else if ( currentYear >= yyyy && currentMonth >= mm )
+      {
+         if ( currentYear > yyyy )
+         {
+            stampNumber = currentYear - yyyy
+            stampString = stampNumber > 1 ? 'Years' : 'Year'
+            return `${stampNumber} ${stampString}`
+         }
+         stampNumber = currentMonth - mm
+         stampString = stampNumber > 1 ? 'Months' : 'Month'
+      }
+
+      result = `${stampNumber} ${stampString}`
+      return result
    }
 
    return (
