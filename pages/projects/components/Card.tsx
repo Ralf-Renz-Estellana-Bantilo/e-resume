9bcc943d71d8fb7db0689c64d1cdf3deafdcb4c2
Ralf Renz Bantilo <ralfrenzbantilo853@gmail.com>
2025-12-07 22:51:30 +0800

    Code refactor

--- pages/projects/components/Card.tsx
+++ pages/projects/components/Card.tsx
@@ -1,11 +1,87 @@
-import { ProjectsInterface } from '@/interfaces';
+import { ProjectsInterface, TagListsInterface } from '@/interfaces';
 import { cardVariants, viewportVariant } from '@/utils/Resources';
 import { Carousel, Divider, Space, Tag } from 'antd';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { findTagByID } from './tagList';
+
+export const tagList: TagListsInterface[] = [
+   {
+      ID: 1,
+      label: 'React JS',
+      color: 'cyan',
+   },
+   {
+      ID: 2,
+      label: 'JavaScript',
+      color: 'yellow',
+   },
+   {
+      ID: 3,
+      label: 'Node JS',
+      color: 'green',
+   },
+   {
+      ID: 4,
+      label: 'MySQL',
+      color: 'orange',
+   },
+   {
+      ID: 5,
+      label: 'TailwindCSS',
+      color: 'blue',
+   },
+   {
+      ID: 6,
+      label: 'Vue JS',
+      color: 'lime',
+   },
+   {
+      ID: 7,
+      label: 'Vuetify',
+      color: 'geekblue',
+   },
+   {
+      ID: 8,
+      label: 'Photoshop',
+      color: 'geekblue',
+   },
+   {
+      ID: 9,
+      label: 'Figma',
+      color: 'orange',
+   },
+   {
+      ID: 10,
+      label: 'Express JS',
+      color: 'yellow',
+   },
+   {
+      ID: 11,
+      label: 'PHP',
+      color: 'orange',
+   },
+   {
+      ID: 12,
+      label: 'Java',
+      color: 'volcano',
+   },
+   {
+      ID: 13,
+      label: 'Next JS',
+      color: 'magenta',
+   },
+   {
+      ID: 14,
+      label: 'TypeScript',
+      color: 'geekblue',
+   },
+];
+
+export const findTagByID = (tagID: Number) => {
+   return tagList.filter((list) => list.ID == tagID)[0];
+};
 
 const Card = () => {
    const mernStack: number[] = [4, 10, 1, 3];
